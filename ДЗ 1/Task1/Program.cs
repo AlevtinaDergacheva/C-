a755c30c5a7a55b12629f45dbf3af883f69fc21f
Alevtina Dergacheva <alia-d15@yandex.ru>
2022-09-06 21:08:46 +0300

    Task1

---
+++ ДЗ 1/Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿//Определение большего и меньшего из двух чисел
+Console.WriteLine ("Введите первое число - ");
+int firstNumber = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine ("Введите второе число - ");
+int secondNumber = Convert.ToInt32 (Console.ReadLine());
+
+if (firstNumber < secondNumber)
+    {
+        Console.Write ("Меньшее число: ");
+        Console.WriteLine (firstNumber);
+        Console.Write ("Большее число: ");
+        Console.WriteLine (secondNumber);
+    }
+else
+    {
+        Console.Write ("Меньшее число: ");
+        Console.WriteLine (secondNumber);
+        Console.Write ("Большее число: ");
+        Console.WriteLine (firstNumber);
+    }
