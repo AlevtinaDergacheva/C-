d58645c33f2eb95e5eb8c1315a9e561d80589bc2
Alevtina Dergacheva <alia-d15@yandex.ru>
2022-09-17 20:18:23 +0300

    Task 34

---
+++ DZ5/Task34/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+
+int[] array = new int[new Random().Next(1, 10)];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine($"Количество четных элементов массива = {EvenNumbers(array)}");
+
+int EvenNumbers (int[] array) 
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++) 
+    if (array[i] % 2 == 0)
+    {
+        count += 1;
+    }
+    return count;
+}
+
+void FillArray(int[] array)
+{
+    int length = array.Length;
+    int index = 0;
+    while (index < length)
+    {
+        array[index] = new Random().Next(100, 1000);
+        index++;
+    }
+}
+
+void PrintArray(int[] array)
+{
+    int length = array.Length;
+    int index = 0;
+    while (index < length)
+    {
+        Console.Write($"{array[index]}, ");
+        index++;
+    }
+}
+
