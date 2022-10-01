6457137316ad2805f55db1e53395947ae5721724
Alevtina Dergacheva <alia-d15@yandex.ru>
2022-10-01 20:36:25 +0300

    Task 41

---
+++ DZ6/Task41/Program.cs
@@ -0,0 +1,46 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Write("Введите число М - ");
+int M = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[M];
+Console.WriteLine("Введите М чисел (элементы массива) - ");
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+Console.Write("Количество чисел больше 0 = ");
+Console.WriteLine(FindNumber(array));
+
+void FillArray(int[] array)
+{
+    int length = array.Length;
+    int index = 0;
+    while (index < length)
+    {
+        array[index] = Convert.ToInt32(Console.ReadLine());
+        index++;
+    }
+}
+void PrintArray(int[] array)
+{
+    int length = array.Length;
+    int index = 0;
+    while (index < length)
+    {
+        Console.Write($"{array[index]}, ");
+        index++;
+    }
+}
+int FindNumber(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            result++;
+        }
+    }
+    return result;
+}
